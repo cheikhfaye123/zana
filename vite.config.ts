c7fe2663e662947658f94023da80bd657fd58c4c
cheikhfaye123 <baboudieye58@gmail.com>
2025-06-10 20:27:56 +0200

    mobile faille encore

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// @ts-ignore
 import ViteRewriteAll from 'vite-plugin-rewrite-all';
 
 export default defineConfig({
   plugins: [
     react(),
-    ViteRewriteAll()
-  ]
+    ViteRewriteAll(),
+  ],
 });
