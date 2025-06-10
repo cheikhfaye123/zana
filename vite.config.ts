ca489c524ce1c9416fa91348ab7aacf170e682b5
cheikhfaye123 <baboudieye58@gmail.com>
2025-06-10 20:24:27 +0200

    mobile faille

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// @ts-ignore
+import ViteRewriteAll from 'vite-plugin-rewrite-all';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  plugins: [
+    react(),
+    ViteRewriteAll()
+  ]
 });
