21180e5f8bb53554701b79d485d772f6b33af6c7
cheikhfaye123 <baboudieye58@gmail.com>
2025-06-10 20:55:09 +0200

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true
-  },
   base: '/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: 'dist'
   }
 })
